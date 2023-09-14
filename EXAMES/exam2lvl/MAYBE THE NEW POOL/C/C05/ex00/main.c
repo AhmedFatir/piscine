79f31e65a3ea854afd0ca330530b2ce979732528
Ahmed Fatir <123210398+AhmedFatir@users.noreply.github.com>
2023-09-14 07:58:10 +0100

    Delete EXAMES/exam2lvl directory

--- EXAMES/exam2lvl/MAYBE THE NEW POOL/C/C05/ex00/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int ft_iterative_factorial(int nb);
-
-int main()
-{
-	printf("%d", ft_iterative_factorial(0));
-}
--- EXAMES/exam2lvl/MAYBE THE NEW POOL/Rush/Rush00/ex00/main.c
+++
@@ -1,7 +0,0 @@
-void	rush(int x, int y);
-
-int		main(void)
-{
-	rush(1,1);
-	return (0);
-}
